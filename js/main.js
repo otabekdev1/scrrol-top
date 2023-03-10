bbfa7b51b928bce61ecd53e28673ab13f792e973
otabekdev1 <githubisomovotabek@gmail.com>
2023-03-10 22:37:42 +0500

    code fixed

--- js/main.js
+++ js/main.js
@@ -1,26 +1,19 @@
-let calcScrollValue = () => {
-  let scrollProgres = document.getElementById("progress");
-  let progressValue = document.getElementById("progress-value");
-  let pos = document.documentElement.scrollTop;
-  let calcHeight =
+const calcScrollValue = () => {
+  const scrollProgres = document.getElementById("progress");
+  const progressValue = document.getElementById("progress-value");
+  const pos = document.documentElement.scrollTop;
+  const calcHeight =
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
-  let scrollValue = Math.round((pos * 100) / calcHeight);
-  if (pos > 100) {
-    scrollProgres.style.display = "grid";
-  } else {
-    scrollProgres.style.display = "none";
-  }
+  const scrollValue = Math.round((pos * 100) / calcHeight);
+
+  pos > 100
+    ? (scrollProgres.style.display = "grid")
+    : (scrollProgres.style.display = "none");
   scrollProgres.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
   scrollProgres.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
   });
 };
-
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
-
-// const dasdas =
-// pos > 100
-//   ? (scrollProgres.style.display = "grid")
-//   : (scrollProgres.style.display = "none");
